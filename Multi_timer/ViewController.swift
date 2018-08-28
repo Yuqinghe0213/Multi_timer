e43dcb7bab5c6d40a3039acb521e313337fda0a5
Yuqinghe0213 <yuqingh2@student.unimelb.edu.au>
2018-08-28 20:49:36 +1000

    Initial Commit

---
+++ Multi_timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Multi_timer
+//
+//  Created by Yuqing on 28/8/18.
+//  Copyright © 2018 Yuqing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
